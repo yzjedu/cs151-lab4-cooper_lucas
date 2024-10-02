75fb9b3cfcf442a25c632bb7eeb0206429b2bf19
ccnazar <ccnazar@loyola.edu>
2024-10-02 16:17:12 -0400

    Code draft

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # Output the purpose of the program
 
 package_type = input('What subscription package do you have? (green, blue, or purple) ')
-package_type = package_type.lower.strip()
+package_type = package_type.lower().strip()
 
 while package_type not in ['green', 'blue', 'purple']:
     print('Sorry, invalid package type.')
     package_type = input('What subscription package do you have? (green, blue, or purple) ')
-    package_type = lower.strip(package_type)
+    package_type = package_type.lower().strip()
 
 monthly_price = 0
 data_provided = 0
@@ -18,6 +18,24 @@ if package_type == 'green':
     monthly_price = float(49.99)
     data_provided = float(2)
     data_used = float(input('How many GB of data did you use?'))
-    coupon = input('Do you have a coupon? (y/n)')
     if data_used > data_provided:
         monthly_price += float(15*(data_used - data_provided))
+        if monthly_price >= 75:
+            coupon = input('Do you have a coupon? (y/n)')
+            while coupon != 'y' and coupon != 'n':
+                print('Sorry, invalid coupon.')
+                coupon = input('Do you have a coupon? (y/n)')
+            if coupon == 'y':
+                monthly_price -= 20
+elif package_type == 'blue':
+    monthly_price = float(70)
+    data_provided = float(4)
+    data_used = float(input('How many GB of data did you use?'))
+    if data_used > data_provided:
+        monthly_price += float(10 * (data_used - data_provided))
+else:
+    monthly_price = float(99.95)
+
+monthly_price = round(monthly_price, 2)
+
+print(f'Your monthly price is: ${monthly_price}')
