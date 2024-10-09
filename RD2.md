8c46d65137714ee0c99a9c84c46ba22c24f896c9
CooperNazar <ccnazar@loyola.edu>
2024-10-08 22:22:12 -0400

    Comments, RD2, test cases fixed -Cooper

--- RD2.md
+++ RD2.md
@@ -1,7 +1,57 @@
 # Reflection Document
 
-* Drive Full Name  : 
-* Partner Full Name: 
-* Student ID: 
+* Drive Full Name  : Cooper Nazar
+* Partner Full Name: Lucas Podowski
+* Student ID: 001930106
 
 
+**Objective:**
+- What were you supposed to learn/accomplish?
+
+We were supposed to get practice with string methods and formatting, decision making, input validation with loops, and writing our own algorithms, flowcharts, and control paths before coding.
+
+
+**Procedure:**
+- What steps were followed and what techniques did you use to solve the problem?
+
+We tried to follow all the steps listed in the README file. We used techniques such as making inputs lowercase to make sure every version of the word would work.
+
+- What were the Key concepts explored?
+
+The key concepts explored were string methods and formatting, decision making, input validation with loops, and writing algorithms, flowcharts, and control paths.
+
+
+**Results:**
+- Did your results match what you expected to get?
+
+The results seemed to match what we expected to get.
+
+- Did you try using various test cases, or extreme test cases?
+
+We tried using various test cases.
+
+
+**Reflection:**
+- What challenges did you encounter?
+
+It took a while to remember how to make a word lowercase, and it was very time-consuming to do all of the if statements.
+
+- How did you follow the first 3 rules of programming?
+
+We thought and discussed before programming, made the program understandable, and practiced our skills while making the program.
+
+- Did you overcome them, and how?
+
+We looked back at past assignments to see how we did things in the past, then used that knowledge to do this assignment.
+
+- Any key takeaways?
+
+It can be helpful to refer to past works to decide how to do something in a current work.
+
+- Do you think you learned what you were supposed to learn for this lab?
+
+I think I learned what I was supposed to learn.
+
+- What was it like working with your partner?
+
+Working with Lucas was great. We managed to get most of the work done in our lab time because we were able to discuss solutions and come to a common understanding.
--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-# Input head comments here ***
+# Programmers: Lucas Podowski, Cooper Nazar
+# Course:  CS151, Zelalem Yalew
+# Due Date: 10/9/2024
+# Lab Assignment: 4
+# Problem Statement: This program is designed to determine how much a customer owes their mobile phone provider based on their subscription package and amount of data used.
+# Data In: Package type, data used, whether the user has a coupon or not
+# Data Out: Monthly price
+# Credits:
 
 # Output the purpose of the program
+print("The purpose of this program is to determine how much a customer owes their mobile phone provider based on their "
+      "subscription package and amount of data used.")
 
+# Prompt the user to input their subscription package type
 package_type = input('What subscription package do you have? (green, blue, or purple) ')
 package_type = package_type.lower().strip()
 
+# Make it so any input other than "green", "blue", or "purple" will not work, and prompt the user to try again
 while package_type not in ['green', 'blue', 'purple']:
     print('Sorry, invalid package type.')
     package_type = input('What subscription package do you have? (green, blue, or purple) ')
     package_type = package_type.lower().strip()
 
+# Set monthly price, data provided, and data used to zero
 monthly_price = 0
 data_provided = 0
 data_used = 0
 
+# If the package type is green, set the monthly price and data provided accordingly, and prompt the user to input data used.
+# If data used is greater than data provided, increase monthly price by 15 for each extra GB of data used
+# If monthly price is greater than or equal to 75, prompt the user to input whether they have a coupon or not
+# If the user has a coupon, subtract 20 from monthly price
 if package_type == 'green':
     monthly_price = float(49.99)
     data_provided = float(2)
@@ -27,13 +43,17 @@ if package_type == 'green':
                 coupon = input('Do you have a coupon? (y/n) ')
             if coupon == 'y':
                 monthly_price -= 20
+# If package type is blue, set the monthly price and data provided accordingly, and prompt the user to input data used.
+# If data used is greater than data provided, increase monthly price by 10 for every extra GB of data used
 elif package_type == 'blue':
     monthly_price = float(70)
     data_provided = float(4)
     data_used = float(input('How many GB of data did you use? '))
     if data_used > data_provided:
         monthly_price += float(10 * (data_used - data_provided))
+# If package type is purple, monthly price is 99.95 with unlimited data
 else:
     monthly_price = float(99.95)
 
+# Output the user's monthly price
 print(f'Your monthly price is: ${monthly_price:.2f}')
